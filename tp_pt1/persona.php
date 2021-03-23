3973dc527a5fa01407642b436630627ea470a1e6
AgustinGaitan <agusgaitan2009@hotmail.com>
2021-03-22 23:26:16 -0300

    clase persona

---
+++ tp_pt1/persona.php
@@ -0,0 +1,52 @@
+<?php 
+
+   abstract class Persona
+   {
+
+      private $_apellido;
+      private $_dni;
+      private $_nombre;
+      private $_sexo;
+
+
+      public function __construct($nombre, $apellido, $dni, $sexo)
+      {
+         $this->_apellido = $apellido;
+         $this->_dni = $dni;
+         $this->_sexo = $sexo;
+         $this->_nombre = $nombre;
+
+      }
+
+      public function GetApellido()
+      {
+         return $this->_apellido;
+      }
+    
+      public function GetDni()
+      {
+         return $this->_dni;
+      }
+
+      public function GetNombre()
+      {
+         return $this->_nombre;
+      }
+
+      public function GetSexo()
+      {
+         return $this->_sexo;
+      }
+
+      abstract function Hablar($idioma);
+
+      public function ToString()
+      {
+         return "NOMBRE: ". $this->GetNombre() . "-" . "APELLIDO: " . "-" . $this->_apellido ."-" . "DNI: " . $this->_dni . "-".
+         "SEXO: " . $this->_sexo;
+      }
+
+
+   }
+    
+
